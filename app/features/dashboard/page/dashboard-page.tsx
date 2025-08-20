b71498ac316f48fee4eb6f424701c6c62389ddbb
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2025-08-20 01:51:21 -0600

    change

--- app/features/dashboard/page/dashboard-page.tsx
+++ app/features/dashboard/page/dashboard-page.tsx
@@ -2,7 +2,14 @@ import Page from '@/components/layout/page'
 import { SidebarProvider } from '@/components/primitives/sidebar'
 import { DashboardSidebar } from '../components/layout'
 import { Outlet } from 'react-router'
+import { useMe } from '@/features/auth/hooks/useMe'
+import { useNavigate } from 'react-router'
 export default function DashboardPage() {
+    const { data: user } = useMe();
+    const navigate = useNavigate()
+
+
+    if (!user) navigate('/auth')
 
 
     return (
