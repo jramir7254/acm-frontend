d1626f064c7b19d25284dc87ac2023076e080e22
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2025-12-14 23:23:12 -0700

    changed queries pt3

--- app/features/users/hooks/user/queries.ts
+++ app/features/users/hooks/user/queries.ts
@@ -1,7 +1,6 @@
 import { backend } from "@/lib/backend-api";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { eventKeys } from "../event-keys";
-import type { EventFields, Event } from "../../types/event";
+import { queryKeys } from "@/lib/query-keys";
 
 
 
@@ -9,8 +8,8 @@ import type { EventFields, Event } from "../../types/event";
 
 export function useUser(reqUserId: string) {
     return useQuery({
-        queryKey: usersKeys.one(reqUserId),
-        queryFn: () => backend.get<User>(`/users/${reqUserId}`, { params: { view: 'full' } }),
+        queryKey: queryKeys.users.detail.base(reqUserId),
+        queryFn: () => backend.get(`/users/${reqUserId}`, { params: { view: 'full' } }),
         staleTime: 60 * 60 * 1000, // 1h fresh
         gcTime: 7 * 24 * 60 * 60 * 1000, // keep cached for 7 days
     });
