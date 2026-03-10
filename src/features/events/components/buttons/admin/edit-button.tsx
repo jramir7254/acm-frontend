11c7b52d031833ef28927ceae5441c716a36aa7d
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2026-03-10 15:05:37 -0600

    fix lol

--- src/features/events/components/buttons/admin/edit-button.tsx
+++ src/features/events/components/buttons/admin/edit-button.tsx
@@ -8,7 +8,7 @@ import { PermissionGuard } from "@/components/layout/permission";
 export function EditEventButton({ event }: { event: Event }) {
 
     return (
-        <PermissionGuard resource="events" requiredActions={["update"]}>
+        <PermissionGuard resource="events" requiredRoles={['president']}>
             <EventFormOverlay event={event} >
                 <Button variant='secondary' size='icon'><Edit /></Button>
             </EventFormOverlay>
